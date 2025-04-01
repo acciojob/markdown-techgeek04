0728a75880b50c88212615033c2a4ef889e4f89e
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-04-01 20:50:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
+import {ReactMarkdown} from "react-markdown";
 import "./App.css";
 
 function Markdown() {
